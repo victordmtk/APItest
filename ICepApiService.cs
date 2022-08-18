ea5358acea08b8648a579567dc8b8ba7ebaeab4c
Victor Demetriuk <victor.dmtk@gmail.com>
2022-08-18 07:45:11 -0300

    adding files

---
+++ ICepApiService.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consumeAPIteste
+{
+    internal interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Refit;
+using System;
+using consumeAPIteste;
+
+class program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {//mapeamento da url
+            var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+            Console.WriteLine("Informe seu Cep:");
+            string cepInformado = Console.ReadLine().ToString();
+            Console.WriteLine("Consultando Informações do CEP: " + cepInformado);
+            var enderecoObtido = await cepClient.GetAddressAsync(cepInformado);
+            Console.WriteLine($"\nLogradouro: {enderecoObtido.Logradouro},\nBairro:{enderecoObtido.Bairro},\nCidade:{enderecoObtido.Localidade}");
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Erro na consulta");
+        }
+    }
+}
+
